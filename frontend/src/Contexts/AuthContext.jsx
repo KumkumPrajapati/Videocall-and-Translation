7330dcb8ec9245d38b416831eb922736796ce6cd
KumkumPrajapati <dakshkumkum8@gmail.com>
2024-09-06 00:04:59 +0530

    add modified file

--- frontend/src/Contexts/AuthContext.jsx
+++ frontend/src/Contexts/AuthContext.jsx
@@ -12,7 +12,7 @@ import { createContext } from "react";
 
 export const AuthContext = createContext({})
 const client = axios.create({
-    baseURL: `${server}/api/v1/users`
+    baseURL: `${server}api/v1/users`
 })
 
 export const AuthProvider = ({children}) =>{
